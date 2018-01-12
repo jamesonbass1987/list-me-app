0898a88a77555359a09403164910bbe3ede69b90
jamesonbass1987 <jamesonbass@gmail.com>
2018-01-11 20:10:23 -0800

    updated seed data and removed redundant rails helper methods

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -4,20 +4,6 @@ module ListingsHelper
     "None (Enter new links below)" if assets.empty?
   end
 
-  def delete_edit_listing_links(listing)
-    if can? :destroy, listing
-      ((link_to "Edit", edit_location_listing_path(@listing.location, @listing), :class => 'btn btn-outline-warning btn-block') + " " +
-      (link_to 'Delete', location_listing_path(@listing.location, @listing), :method => :delete, :class => 'btn btn-outline-danger btn-block'))
-    end
-  end
-
-  def display_owner_listing_controls(user, listing)
-    if can? :manage, listing
-      ((link_to 'Edit', edit_location_listing_path(listing.location, listing.id), :class=>"btn btn-outline-warning") + " " +
-      (link_to 'Delete', location_listing_path(listing.location, listing), :method => :delete, :class=>"btn btn-outline-danger"))
-    end
-  end
-
   def create_listing_link(location)
     if can? :create, Listing
        link_to("Create Listing", new_location_listing_path(location), :class => 'btn btn-outline-success btn-block')
--- db/seeds.rb
+++ db/seeds.rb
@@ -32,6 +32,33 @@ listing_1.listing_images.create(image_url: "https://images-na.ssl-images-amazon.
 listing_1.listing_images.create(image_url: "https://images-na.ssl-images-amazon.com/images/I/5178R%2B%2BFiwL._SL1500_.jpg")
 
 
+listing_2 = Listing.create(
+    title: "Squier by Fender Stratocaster Pack with Frontman 10G Amp, Cable, Strap, Picks, and Online Lessons",
+    description: 'A perfect choice for beginners, the new Squier Strat Pack has everything you need to begin playing right out of the box. The short-scale Stratocaster (24") is ideal for players with smaller hands and provides a comfortable playing feel. Other features include a lightweight body, a hardtail bridge for rock-solid tuning and three single-coil pickups for classic Strat tone. The included Squier Frontman 10G amplifier is the perfect companion for jamming thanks to its aux input that allows you play along with your favorite songs or backing tracks, as well as a headphone jack for silent practice. The Squier Starter Strat Pack also comes with a strap, cable and picks -- everything you need to stop dreaming and start playing.',
+    price: "129.99",
+    location_id: '2',
+    category_id: '26',
+    user_id: user_ids[rand(0..4)]
+
+)
+
+
+listing_2.tags.find_or_create_by(name: "guitar")
+listing_2.tags.find_or_create_by(name: "squier")
+listing_2.tags.find_or_create_by(name: "music")
+listing_2.tags.find_or_create_by(name: "fender")
+listing_2.tags.find_or_create_by(name: "electric guitar")
+listing_2.tags.find_or_create_by(name: "cutaway guitar")
+listing_2.tags.find_or_create_by(name: "custom made")
+listing_2.tags.find_or_create_by(name: "black")
+listing_2.tags.find_or_create_by(name: "pickups")
+
+listing_2.listing_images.create(image_url: "https://images-na.ssl-images-amazon.com/images/I/71ELNDf7Y%2BL._SL1280_.jpg")
+listing_2.listing_images.create(image_url: "https://images-na.ssl-images-amazon.com/images/I/61dI6ynBIGL._SL1500_.jpg")
+listing_2.listing_images.create(image_url: "https://images-na.ssl-images-amazon.com/images/I/71yVvwNK35L._SL1500_.jpg")
+listing_2.listing_images.create(image_url: "https://images-na.ssl-images-amazon.com/images/I/71zTesqnEpL._SL1500_.jpg")
+listing_2.listing_images.create(image_url: "https://images-na.ssl-images-amazon.com/images/I/5178R%2B%2BFiwL._SL1500_.jpg")
+
 listing_3 = Listing.create(
     title: "Echo Dot (2nd Generation) - Black",
     description: 'Echo Dot (2nd Generation) is a hands-free, voice-controlled device that uses Alexa to play music, control smart home devices, make calls, send and receive messages, provide information, read the news, set music alarms, read audiobooks from Audible, control Amazon Video on Fire TV, and more
@@ -42,37 +69,6 @@ listing_3 = Listing.create(
     price: "49.99",
     location_id: '2',
     category_id: '25',
-    user_id: user_ids[rand(0..4)]
-
-)
-
-
-listing_3.tags.find_or_create_by(name: "guitar")
-listing_3.tags.find_or_create_by(name: "squier")
-listing_3.tags.find_or_create_by(name: "music")
-listing_3.tags.find_or_create_by(name: "fender")
-listing_3.tags.find_or_create_by(name: "electric guitar")
-listing_3.tags.find_or_create_by(name: "cutaway guitar")
-listing_3.tags.find_or_create_by(name: "custom made")
-listing_3.tags.find_or_create_by(name: "black")
-listing_3.tags.find_or_create_by(name: "pickups")
-
-listing_3.listing_images.create(image_url: "https://images-na.ssl-images-amazon.com/images/I/71ELNDf7Y%2BL._SL1280_.jpg")
-listing_3.listing_images.create(image_url: "https://images-na.ssl-images-amazon.com/images/I/61dI6ynBIGL._SL1500_.jpg")
-listing_3.listing_images.create(image_url: "https://images-na.ssl-images-amazon.com/images/I/71yVvwNK35L._SL1500_.jpg")
-listing_3.listing_images.create(image_url: "https://images-na.ssl-images-amazon.com/images/I/71zTesqnEpL._SL1500_.jpg")
-listing_3.listing_images.create(image_url: "https://images-na.ssl-images-amazon.com/images/I/5178R%2B%2BFiwL._SL1500_.jpg")
-
-listing_3 = Listing.create(
-    title: "Echo Dot (2nd Generation) - Black",
-    description: 'Echo Dot (2nd Generation) is a hands-free, voice-controlled device that uses Alexa to play music, control smart home devices, make calls, send and receive messages, provide information, read the news, set music alarms, read audiobooks from Audible, control Amazon Video on Fire TV, and more
-    Connects to speakers or headphones through Bluetooth or 3.5 mm stereo cable to play music from Amazon Music, Spotify, Pandora, iHeartRadio, and TuneIn. Play music simultaneously across Echo devices and speakers connected via cable with multi-room music.
-    Call or message almost anyone hands-free with your Echo device. Also, instantly connect to other Echo devices in your home using just your voice.
-    Controls lights, fans, TVs, switches, thermostats, garage doors, sprinklers, locks, and more with compatible connected devices from WeMo, Philips Hue, Sony, Samsung SmartThings, Nest, and others
-    Hears you from across the room with 7 far-field microphones for hands-free control, even in noisy environments or while playing music',
-    price: "49.99",
-    location_id: '2',
-    category_id: rand(21..32),
     user_id: user_ids[rand(0..4)]
 )
 
